94d484eb9461494e92a554fe228fb13cdef7a327
Muhammad Aref <muh.aref@live.com>
2016-07-01 13:13:09 +0200

    Add new test cases for stack implementation using JavaScript

--- stack/javascript/stack_object.js
+++ stack/javascript/stack_object.js
@@ -3,7 +3,7 @@
  */
  
 function Stack (capacity) {
-    if(capacity <= 0) throw new Error('Invalid capacity value: ' + capacity);
+    if (capacity <= 0) throw new Error('Invalid capacity value: ' + capacity);
     
     this._capacity = capacity || Infinity;
     this._storage = {};
@@ -11,15 +11,17 @@ function Stack (capacity) {
 }
 
 Stack.prototype.push = function (elem) {
-    if(this._size === this._capacity) throw new Error('The stack is filled, remove elements before adding new');
+    if (this._size === this._capacity) throw new Error('The stack is filled, remove elements before adding new');
 
     this._storage[this._size++] = elem;
 }
 
 Stack.prototype.pop = function () {
-   var elem = this._storage[--this._size];
-   delete this._storage[this._size];
-   return elem;
+    if (this._size) {
+        var elem = this._storage[--this._size];
+        delete this._storage[this._size];
+        return elem;
+    }
 }
 
 Stack.prototype.size = function () {
--- stack/javascript/stack_spec.js
+++ stack/javascript/stack_spec.js
@@ -5,7 +5,7 @@ describe('Stack DT using Object as a storage', function () {
 		Stack = require('./stack_object');
 	});
 
-	it('Should show an error with negative capacity values', function () {
+	it('Should throw an error with negative capacity values', function () {
 		try {
 			var stack = new Stack(-9);
 		} catch (e) {
@@ -20,5 +20,34 @@ describe('Stack DT using Object as a storage', function () {
 		stack.push('c');
 
 		expect(stack.size()).toBe(3);
+	});
+
+	it('Should return undefined if the stack is empty', function () {
+		var stack = new Stack();
+
+		expect(stack.pop()).toBeUndefined();
+	});
+
+
+	it('Should throw an error with adding element into filled stack', function () {
+		var stack = new Stack(1);
+		stack.push(1);
+
+		try {
+			stack.push(2);
+		} catch (e) {
+			expect(e.message).toBe('The stack is filled, remove elements before adding new');
+		}
+	});
+
+	it('Should return the most top element', function () {
+		var stack = new Stack();
+		stack.push(1);
+		stack.push(2);
+		stack.push(3);
+
+		expect(stack.pop()).toBe(3);
+		expect(stack.pop()).toBe(2);
+		expect(stack.pop()).toBe(1);
 	});
 });
