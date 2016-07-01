0573d168c75772528bd58a70a0575d9047ae5404
Muhammad Aref <muh.aref@live.com>
2016-07-01 12:13:36 +0200

    Add new test cases for stack implementation using Python

--- stack/python/stack_dict.py
+++ stack/python/stack_dict.py
@@ -15,14 +15,22 @@ class Stack:
 		self.__size = 0
 		
 	def push(self, elem):
+		try:
+			if self.__size == self.__capacity:
+				raise MemoryError('The stack is filled, remove elements before adding new')
+		except MemoryError as error:
+			print(error)
+
 		self.__storage[self.__size] = elem
 		self.__size += 1
 		
 	def pop(self):
-		elem = self.__storage.get(self.__size - 1)
-		self.__size -= 1
-		del(self.__storage[self.__size])
-		return elem
+		if self.__size:
+			elem = self.__storage.get(self.__size - 1)
+			self.__size -= 1
+			del(self.__storage[self.__size])
+			return elem
 		
+	@property
 	def size(self):
 		return self.__size
--- stack/python/test_stack.py
+++ stack/python/test_stack.py
@@ -2,11 +2,38 @@ import unittest
 from .stack_dict import Stack
 
 class StackWithDictTest(unittest.TestCase):
-	def test_negative_capacty_values(self):
+	def test_negative_capacity_values(self):
 		try:
 			stack = Stack(-9)
 		except ValueError as error:
 			self.assertEqual(error, 'Invalid capacity value: -9')
+
+	def test_empty_stack(self):
+		stack = Stack()
+		self.assertEqual(stack.pop(), None)
+
+	def test_stack_size(self):
+		stack = Stack()
+		stack.push(1)
+		stack.push(2)
+		stack.push(3)
+		self.assertEqual(stack.size, 3)
+
+	def test_stack_pop(self):
+		stack = Stack()
+		stack.push(1)
+		stack.push(2)
+		stack.push(3)
+		self.assertEqual(stack.pop(), 3)
+
+	def test_filled_stack(self):
+		stack = Stack(1)
+		stack.push(1)
+
+		try:
+			stack.push(2)
+		except MemoryError as error:
+			self.assertEqual(error, 'The stack is filled, remove elements before adding new')
 
 if __name__ == '__main__':
     unittest.main()
